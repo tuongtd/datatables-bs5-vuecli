fcaac2321f8a57996654b1475145b260e1046244
TuongTD <tuongtd@adtrue.com>
2022-08-23 15:33:04 +0700

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,69 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div class="container">
+		<h1>Bootstrap 53 styled DataTable</h1>
+		<h2>DataTables + Vue3 example</h2>
+		<p>
+			In this example we show a DataTable in a Vue3 application being styled by
+			Bootstrap 5. DataTables supports a number of other styling libraries as
+			well, such as Bulma, Foundation, Boostrap 3, 4 and others.
+		</p>
+
+		<DataTable
+			:columns="columns"
+			:data="[]"
+			class="table table-hover table-striped"
+			width="100%"
+		>
+			<thead>
+			<tr>
+				<th>Name2</th>
+				<th>Position</th>
+				<th>Office</th>
+				<th>Extn.</th>
+				<th>Start date</th>
+				<th>Salary</th>
+			</tr>
+			</thead>
+			<tfoot>
+			<tr>
+				<th>Name</th>
+				<th>Position</th>
+				<th>Office</th>
+				<th>Extn.</th>
+				<th>Start date</th>
+				<th>Salary</th>
+			</tr>
+			</tfoot>
+		</DataTable>
+	</div>
 </template>
 
 <script>
+import DataTable from 'datatables.net-vue3';
+import DataTableBs5 from 'datatables.net-bs5';
+
+DataTable.use(DataTableBs5);
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+	components: {
+		DataTable
+	},
+	data() {
+		return {
+			columns: [
+				{ data: 'name' },
+				{ data: 'position' },
+				{ data: 'office' },
+				{ data: 'extn' },
+				{ data: 'start_date' },
+				{ data: 'salary' },
+			]
+		}
+	}
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+@import 'bootstrap';
+@import 'datatables.net-bs5';
 </style>
